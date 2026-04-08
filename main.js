5261fbc8eb9b00844b5bb28a143679b6c7dc12e7
Khainesus <akke2@seznam.cz>
2026-04-08 10:38:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,38 @@
  * @returns {Array} of employees
  */
 export function main(dtoIn) {
-  //TODO code
-  //let dtoOut = exMain(dtoIn);
-  return dtoOut;
+  
+  const maleNames = ["Jakub", "Tomáš", "Jan", "Martin", "Petr", "Lukáš", "Ondřej", "David", "Michal", "Pavel",
+    "Jiří", "Radek", "Marek", "Filip", "Václav", "Stanislav", "Roman", "Miroslav", "Zdeněk", "Karel",
+    "Jaroslav", "Josef", "Vojtěch", "Adam", "Dominik", "Patrik", "Richard", "Tomáš", "Libor", "Miloslav",
+    "René", "Vladimír", "Antonín", "Bohumil", "Čestmír", "Dalibor", "Eduard", "František", "Gustav", "Hynek",
+    "Igor", "Jindřich", "Kamil", "Ladislav", "Milan"];
+
+  const femaleNames = ["Jana", "Tereza", "Lucie", "Markéta", "Petra", "Kateřina", "Veronika", "Martina", "Eva", "Lenka",
+    "Monika", "Alena", "Ivana", "Hana", "Zuzana", "Michaela", "Barbora", "Kristýna", "Renata", "Simona",
+    "Dagmar", "Eliška", "Gabriela", "Helena", "Ilona", "Jitka", "Kamila", "Ludmila", "Olga",
+    "Pavlína", "Radka", "Uršula", "Věra", "Xenie", "Yvona", "Žaneta", "Blanka",
+    "Andrea", "Božena", "Cecílie", "Denisa", "Irena", "Jaroslava", "Marie", "Růžena"];
+
+  const maleSurnames = ["Novák", "Svoboda", "Novotný", "Dvořák", "Černý", "Procházka", "Kučera", "Veselý", "Horák", "Němec",
+    "Pospíšil", "Marek", "Pokorný", "Hájek", "Jelínek", "Král", "Růžička", "Beneš", "Fišer", "Sedláček",
+    "Doležal", "Zeman", "Kolář", "Navrátil", "Čermák", "Urban", "Blaha", "Kozák", "Kratochvíl", "Kovář",
+    "Málek", "Stehlík", "Vlček", "Horáček", "Šimánek", "Tůma", "Bureau", "Liška", "Sýkora", "Ptáček"];
+
+  const femaleSurnames = ["Nováková", "Svobodová", "Novotná", "Dvořáková", "Černá", "Procházková", "Kučerová", "Veselá", "Horáková", "Němcová",
+    "Pospíšilová", "Marková", "Pokorná", "Hájková", "Jelínková", "Králová", "Růžičková", "Benešová", "Fišerová", "Sedláčková",
+    "Doležalová", "Zemanová", "Kolářová", "Navrátilová", "Čermáková", "Urbanová", "Blahová", "Kozáková", "Kratochvílová", "Kovářová",
+    "Málková", "Stehlíková", "Vlčková", "Horáčková", "Šimánková", "Tůmová", "Lišková", "Sýkorová", "Ptáčková",
+    "Váchová"];
+
+  const workloads = [10, 20, 30, 40];
+
+  const employees = [];
+
+  return employees;
+}
+
+
+return dtoOut;
 }
 
