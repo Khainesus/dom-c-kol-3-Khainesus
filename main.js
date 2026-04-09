d9ea37b6f6c35bdd4b8b7730b66e021c58dba1d2
Khainesus <akke2@seznam.cz>
2026-04-09 11:22:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 //TODO add imports if needed
 //TODO doc
 /**
- * The main function which calls the application. 
+ * The main function which calls the application.
  * Please, add specific description here for the application purpose.
  * @param {object} dtoIn contains count of employees, age limit of employees {min, max}
  * @returns {Array} of employees
  */
-export function main(dtoIn) {
-  //TODO code
-  //let dtoOut = exMain(dtoIn);
-  return dtoOut;
-}
+import { generateGender, generateName, generateSurname, generateWorkload, generateBirthdate } from "./src/employees.js";
 
+export function main(dtoIn) {
+  const employees = [];
+
+  for (let i = 0; i < dtoIn.count; i++) {
+    const gender = generateGender();
+    const name = generateName(gender);
+    const surname = generateSurname(gender);
+    const workload = generateWorkload();
+    const birthdate = generateBirthdate(dtoIn.age.min, dtoIn.age.max);
+
+    employees.push({
+      gender: gender,
+      name: name,
+      surname: surname,
+      birthdate: birthdate,
+      workload: workload,
+    });
+  }
+
+  return employees;
+}
